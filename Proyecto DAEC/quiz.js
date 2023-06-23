5a776b697d4fbd65f667df8f8c539e96e0a5ff09
yaelitohermosito1000 <137457262+yaelitohermosito1000@users.noreply.github.com>
2023-06-22 19:24:49 -0600

    master

---
+++ Proyecto DAEC/quiz.js
@@ -0,0 +1,105 @@
+var questionBank= [
+    {
+        question : 'Today is _____ (cold) than yerterday',
+        option : ['cold','frio','colder','no se '],
+        answer : 'colder'
+    },
+    {
+        question : 'China is _______(big) than Italy',
+        option : ['big','bigger','leap','jump'],
+        answer : 'bigger'
+    },
+    {
+        question : 'She is________young than her brother.',
+        option : ['joven','youth','younger','youthful'],
+        answer : 'younger'
+    },
+    {
+        question : 'He is _______(old) than his friend. ',
+        option : ['older','viejo','aged','elder'],
+        answer : 'older'
+    },
+    {
+        question : 'I stayed___________(long) than I expected',
+        option : ['se me fue xd','viejo','length','longer'],
+        answer : '1967'
+
+    }
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
---
+++ Proyecto DAEC/redirecionarapaginas.js
@@ -0,0 +1,31 @@
+function redireccionarcom() {
+    window.location.href = "ComparativesPaginaInicio.html";
+}
+function redireccionarsup() {
+    window.location.href = "Superlatives.html";
+}
+function redireccionarint() {
+    window.location.href = "intencifiers.html";
+}
+function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}
+function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}
+function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}
+function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}
+function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}
+function redireccionar() {
+    window.location.href = "otra_pagina.html";
+}
